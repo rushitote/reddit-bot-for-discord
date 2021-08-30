3b1ce02738829d06d3c50082ecec130dfb0055b0
Rushikesh Tote <rushi.tote@gmail.com>
2021-08-30 13:00:14 +0530

    added bot

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const dotenv = require("dotenv");
+const reddit = require("./reddit");
+dotenv.config();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let postReply = async (msg, subreddit) => {
+  let embedObject = await reddit.getPost(subreddit);
+  if (embedObject.error) {
+    msg.reply(
+      "Sorry, either the subreddit is not accessible or an internal server error."
+    );
+    return;
+  }
+
+  if (embedObject.over_18) {
+    msg.reply("Not allowed here.");
+    return;
+  }
+
+  const embed = new Discord.MessageEmbed();
+  embed.setTitle(embedObject.title);
+  embed.setFooter(embedObject.sub);
+  embed.setURL(embedObject.link);
+  embed.setTimestamp();
+  embed.setColor('#ff4500')
+
+  if (
+    embedObject.thumbnail != "self" &&
+    embedObject.thumbnail != "" &&
+    embedObject.thumbnail != null &&
+    embedObject.thumbnail != "default"
+  ) {
+    embed.setThumbnail(embedObject.thumbnail);
+  }
+
+  if (embedObject.text != "self" && embedObject.text != "") {
+    if (embedObject.text.length > 4000) {
+      embed.setDescription(embedObject.text.slice(0, 4000) + "...");
+    } else {
+      embed.setDescription(embedObject.text);
+    }
+  }
+
+  if (embedObject.image) {
+    embed.setImage(embedObject.image);
+  }
+
+  msg.reply({ embeds: [embed] });
+  if (embedObject.video) {
+    msg.reply(embedObject.video);
+  }
+};
+
+client.on("messageCreate", (msg) => {
+  if (msg.content.toLowerCase().startsWith("red ")) {
+    let words = msg.content.toLowerCase().split(" ");
+    let subreddit = words[1];
+
+    (async () => {
+      try {
+        await postReply(msg, subreddit);
+      } catch {
+        msg.reply("Sorry, something went wrong.")
+      }
+    })();
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
---
+++ reddit.js
@@ -0,0 +1,83 @@
+"use strict";
+const snoowrap = require("snoowrap");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const r = new snoowrap({
+  userAgent: "Reddit Bot by u/" + process.env.REDDIT_USERNAME,
+  clientId: process.env.REDDIT_PERSONAL_USE_SCRIPT,
+  clientSecret: process.env.REDDIT_SECRET,
+  refreshToken: process.env.REDDIT_REFRESH_TOKEN,
+  accessToken: process.env.REDDIT_ACCESS_TOKEN,
+});
+
+let getTopPost = async (subreddit) => {
+  let postsFunc = async (posts) => {
+    let rndPost = Math.floor(Math.random() * posts.length);
+    let post = posts[rndPost];
+    let obj = {};
+
+    if (post.over_18) {
+      obj.over_18 = true;
+    }
+
+    if (post.thumbnail) {
+      obj.thumbnail = post.thumbnail;
+    }
+
+    // if post is image
+    if (
+      post.url.endsWith(".png") ||
+      post.url.endsWith(".jpg") ||
+      post.url.endsWith(".jpeg")
+    ) {
+      obj.image = post.url;
+    }
+    // if post is reddit video
+    else if (post.is_video) {
+      if (post.url.includes("v.redd.it")) {
+        obj.video = post.media.reddit_video.fallback_url;
+      }
+    } 
+    // if post is video
+    else if (
+      post.url.endsWith(".mp4") ||
+      post.url.endsWith(".gifv") ||
+      post.url.endsWith(".gif") ||
+      post.url.includes("gfycat") ||
+      post.url.includes("youtu.be")
+    ) {
+      obj.video = post.url;
+    }
+
+    obj.text = post.selftext;
+    obj.title = post.title;
+    obj.sub = post.subreddit_name_prefixed;
+    obj.link = "https://reddit.com" + post.permalink;
+    return obj;
+  };
+
+  try {
+    let x = await r.getTop(subreddit, { limit: 50, time: "week" });
+    let obj = await postsFunc(x);
+    log(obj);
+    return obj;
+  } catch {
+    let obj = {
+      error: true,
+    };
+    return obj;
+  }
+};
+
+let log = (obj) => {
+  console.log(obj);
+};
+
+// test module
+// (async () => {
+//   let obj = await getTopPost("python");
+//   console.log(obj);
+// })();
+
+module.exports.getPost = getTopPost;
